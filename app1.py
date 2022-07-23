3bf0ce1f1b0c75685ad14f8d4bb0021c60bfd085
chetas562 <70834532+chetas562@users.noreply.github.com>
2022-07-23 11:55:35 +0530

    Add files via upload
    classifier is classifier

---
+++ app1.py
@@ -0,0 +1,17 @@
+from urllib import request
+from flask import Flask, jsonify, request
+from classifier import get_prediction
+
+app=Flask(_name_)
+
+@app.route('/predict-digit', methods=['POST'])
+
+def predict_data():
+    image=request.files.get('digit')
+    prediction=get_prediction(image)
+    return jsonify({
+        'prediction': prediction
+    }),200
+
+if __name__=='__main__':
+    app.run(debug=True)
